68ec36a2010ece2903836d115680d16a401b2a17
Wettergrund <jonas.wettergrund@gmail.com>
2022-11-12 18:42:34 +0100

    Added load effect when fetching from github

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -23,19 +23,25 @@ const reposToShow = ["Weather-app" , "memory"]
 
 
 async function getGit(){
+    const loadingMessage = document.querySelector('#loading')
 
+    loadingMessage.classList.remove('not-visible')
 
     const response = await fetch('https://api.github.com/users/wettergrund/repos');
     const data = await response.json();
 
-    // console.log(data)
+    // delay to demo loading effect
+    setTimeout(() => {displayGit(data)
 
-    displayGit(data)
+    loadingMessage.classList.add('not-visible')
+}, 2000);
+
 }
 
 function displayGit(data){
 
     const mainContainer = document.querySelector(".projects");
+    mainContainer.textContent = "";
     
     for (let i = 0; i < data.length; i++) {
         const { id, name, description , languages_url , html_url}  = data[i];
@@ -45,19 +51,21 @@ function displayGit(data){
 
         if(reposToShow.includes(name)){
 
+            
+
             generateElement('article' , '' , mainContainer, 'class' , 'card beige' , 'data-contid' , id);
             const cardContainer = document.querySelector('[data-contid="' + id + '"]');
 
-            const infoId = 'info-' + name;
+            // time out to demo loading effect
+       
 
-            generateElement('img' , '' , cardContainer , 'src' , 'images/' + name.toLowerCase() + '.jpg')
+            generateElement('img' , '' , cardContainer , 'src' , 'images/' + name.toLowerCase() + '.jpg' , 'alt' , 'bild på ' + name)
 
             generateElement('div' , '' , cardContainer , 'data-infoid' , id);
             const infoContainer = document.querySelector('[data-infoid="' + id + '"]');
 
             generateElement('h3' , description , infoContainer);
             generateElement('a' , "Till repo" , infoContainer , 'href' , html_url , 'class' , 'btn small-btn' , 'target' , '_blank' );
-
 
             
         }
